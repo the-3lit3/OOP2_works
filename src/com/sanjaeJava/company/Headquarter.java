7d267d80ec2d6e5656c0b0a0d4c061f46402211c
S. Facey Fotos <S. Facey Fotos@LAPTOP-K0D1IIFU>
2022-02-24 14:32:08 -0500

    Commit changes to lab

---
+++ src/com/sanjaeJava/company/Headquarter.java
@@ -0,0 +1,9 @@
+package com.sanjaeJava.company;
+
+public class Headquarter extends Office{
+
+
+    public Headquarter(String officeAddress, Departments nDept) {
+        super(officeAddress, nDept);
+    }
+}
